57ba52ad4bbb6fa6728a2b3158792292574b3ec3
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-20 15:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+let level = document.getElementById('level');
+let parentElement = level.parentNode;
+let children = Array.from(parentElement.children); // Convert HTMLCollection to an array
+let index = children.indexOf(level); // Get the index of the level element
+alert(`The level of the element is: ${index} `);
